77ed82b88630f62f096e78df0e5338ce430ef973
Nadezhda <bulygina_n_a@mail.ru>
2024-01-19 19:14:59 +0300

    pars_1.py

--- pars_1.py
+++ pars_1.py
@@ -1,5 +1,4 @@
 import json
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj1 = json.loads(json_text)
-
 print(obj1["messages"])
