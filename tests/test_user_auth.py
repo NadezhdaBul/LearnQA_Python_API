9a0cf2e982ccd1465992e04b0f85e10950612cd2
Nadezhda <bulygina_n_a@mail.ru>
2024-02-03 22:58:04 +0300

    test_user_auth

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,6 +1,7 @@
 import requests
 import pytest
 from lib.base_case import BaseCase
+from lib.assertions import Assertions
 class TestUserAuth (BaseCase):
     exclude_params = [
         ('no_cookie'),
@@ -24,9 +25,34 @@ class TestUserAuth (BaseCase):
             headers={'x-csrf-token':self.token},
             cookies={'auth_sid':self.auth_sid}
         )
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_authmethod,
+            "User id from auth method is not equal from user id chek method"
+        )
 
-        assert "user_id" in response2.json() , 'не юзер ид во втором респонсе'
-        user_id_from_check_method = response2.json()["user_id"]
-        print(user_id_from_check_method)
+        # assert "user_id" in response2.json() , 'не юзер ид во втором респонсе'
+        # user_id_from_check_method = response2.json()["user_id"]
+        # print(user_id_from_check_method)
+        #
+        # assert self.user_id_from_authmethod == user_id_from_check_method, "юзер ид в фактическом результате не соответствует ожидаемому результату"
 
-        assert self.user_id_from_authmethod == user_id_from_check_method, "юзер ид в фактическом результате не соответствует ожидаемому результату"
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+        if condition == "no cookie":
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={"x-csrf-token":self.token}
+            )
+        else:
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                cookies={"auth_sid": self.auth_sid}
+            )
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is outhorized with condition {condition}"
+        )
