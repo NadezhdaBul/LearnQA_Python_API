d59527b8d1224a8558659fd1fd95d969bf1dd12c
Nadezhda <bulygina_n_a@mail.ru>
2024-02-04 11:48:51 +0300

    test_user_register3

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -5,14 +5,10 @@ from lib.assertions import Assertions
 
 
 class TestUserRegister(BaseCase):
-    emails = [
-        ('vinkotov@example.com'),
-        ('vinkotovexample.com')
-    ]
 
-    @pytest.mark.parametrize('email', emails)
-    def test_create_user_with_existing_email(self, email):
 
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
         data = {
             'password': '123',
             'username': 'leanqa',
@@ -26,15 +22,22 @@ class TestUserRegister(BaseCase):
         assert response.status_code == 400, f"Unexpected status code {response.status_code}"
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",  f"Unexpected response content {response.content}"
 
+    def test_create_user_with_invalid_format_email(self):
+        email = 'vinkotovexample.com'
+        data = {
+            'password': '123',
+            'username': 'leanqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
 
-    datas = [
-        ({"'password': '123','username': 'leanqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'qwerty@example.com'"}),
-        ({"'password': '123', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'qwerty@example.com'"}),
-        ("'password': '123', 'username': 'leanqa','lastName': 'learnqa', 'email': 'qwerty@example.com'"),
-        ("'password': '123', 'username': 'leanqa', 'firstName': 'learnqa', 'email': 'qwerty@example.com'"),
-        ("'password': '123','username': 'leanqa', 'firstName': 'learnqa', 'lastName': 'learnqa'")
-    ]
-    #
+        response = requests.post('https://playground.learnqa.ru/api/user', data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode("utf-8") == 'Invalid email format', f"The email {email} format is correct"
+
+
     @pytest.mark.parametrize('data', [
         ({'username': 'leanqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'qwerty@example.com'}),
         ({'password': '123', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'qwerty@example.com'}),
