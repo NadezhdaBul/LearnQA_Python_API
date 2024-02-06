27d7837ad0177bbc4422282520d0d15f3c99c0fc
Nadezhda <bulygina_n_a@mail.ru>
2024-02-06 21:22:27 +0300

    test_user_get2

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -6,16 +6,24 @@ from lib.assertions import Assertions
 
 class TestUserRegister(BaseCase):
 
+    def test_create_user_successfully(self):
+        data = self.prepare_registration_data()
+        response = requests.post('https://playground.learnqa.ru/api/user', data = data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, 'id')
+
+
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'leanqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
+        #     {
+        #     'password': '123',
+        #     'username': 'leanqa',
+        #     'firstName': 'learnqa',
+        #     'lastName': 'learnqa',
+        #     'email': email
+        # }
 
         response = requests.post('https://playground.learnqa.ru/api/user', data = data)
 
