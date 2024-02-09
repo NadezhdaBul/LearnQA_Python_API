2cb40fb412d6b6038005900dbf19cf21110ebf68
Nadezhda <bulygina_n_a@mail.ru>
2024-02-09 18:58:59 +0300

    tags

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,6 +2,10 @@ import requests
 import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
+
+@allure.epic("User authorization cases")
+@allure.issue('https://playground.learnqa.ru')
 class TestUserAuth (BaseCase):
     exclude_params = [
         ('no_cookie'),
@@ -19,6 +23,9 @@ class TestUserAuth (BaseCase):
         self.token = self.get_header(response1, 'x-csrf-token')
         self.user_id_from_authmethod = self.get_json_value(response1, 'user_id')
 
+    @allure.severity("Critical")
+    @allure.title("test_authorization_user_successfully")
+    @allure.testcase("SWT-2253")
     def test_auth_user (self):
         response2 = requests.get(
             'https://playground.learnqa.ru/api/user/auth',
@@ -39,6 +46,9 @@ class TestUserAuth (BaseCase):
         # assert self.user_id_from_authmethod == user_id_from_check_method, "юзер ид в фактическом результате не соответствует ожидаемому результату"
 
     @pytest.mark.parametrize('condition', exclude_params)
+
+    @allure.tag("negative")
+    @allure.title("test_negative_auth_check")
     def test_negative_auth_check(self, condition):
         if condition == "no cookie":
             response2 = requests.get(
